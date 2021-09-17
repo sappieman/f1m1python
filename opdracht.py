0cf17f9e988094fa4e605d422fabf3ff816785be
bas klaichid <basklaichid@gmail.com>
2021-09-17 10:14:02 +0200

    extra print line

---
+++ opdracht.py
@@ -0,0 +1,12 @@
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+print("Hello you!, ik ben python \n")
+c = input("Wie ben jij?\n")
+print(f"Hello {c}\n")
+input()
+
+print("sheeesshhhh")
